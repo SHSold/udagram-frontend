d4292e9608b9b7a9ead588c2a5f18b968e56b6f2
Stefan Sold <ssold@web.de>
2021-12-29 20:08:09 +0100

    take the external-IP of the reverseproxy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'a58ce3305503944338ab6c4acc2de1d1-1927570429.us-east-2.elb.amazonaws.com'
 };
 
 /*
